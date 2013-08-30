cab3c7c00176effdf0976b41552621819390a935
edipotrebol <edipotrebol@gmail.com>
2013-08-30 15:29:14 +0200

    Autostart nm-applet

--- rc.lua
+++ rc.lua
@@ -555,3 +555,6 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Autostart Apps
+os.execute("nm-applet &")
